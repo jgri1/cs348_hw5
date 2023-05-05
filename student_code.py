8e1fd2cb29e7e51613052f05b2f50d2179532e4a
jgri1 <97050200+jgri1@users.noreply.github.com>
2023-05-05 18:22:57 -0500

    Add files via upload

---
+++ student_code.py
@@ -0,0 +1,16 @@
+import common
+
+#helpful, but not needed
+class variables:
+	counter=0
+
+
+def sudoku_backtracking(sudoku):
+	variables.counter = 0
+	#put your code here
+	return variables.counter
+
+def sudoku_forwardchecking(sudoku):
+	variables.counter = 0
+	#put your code here
+	return variables.counter
